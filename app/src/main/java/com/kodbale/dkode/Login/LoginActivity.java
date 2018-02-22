d39a703982810bec422867389a5cbf5c03ed4c6a
Deepak Chethan <deepakchethan@outlook.com>
2018-02-22 19:49:49 +0000

    First Thing

---
+++ app/src/main/java/com/kodbale/dkode/Login/LoginActivity.java
@@ -0,0 +1,57 @@
+package com.kodbale.dkode.Login;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.kodbale.dkode.MainActivity;
+import com.kodbale.dkode.R;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private  EditText groupPass,groupId;
+    private Button btn;
+    private FirebaseAuth mAuth;
+    private String email,password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        groupId = (EditText) findViewById(R.id.group_id);
+        groupPass = (EditText) findViewById(R.id.group_pass);
+        btn = (Button) findViewById(R.id.loginButton);
+        btn.setOnClickListener(this);
+    }
+    
+    @Override
+    public void onClick(View v) {
+        email = groupId.getText().toString();
+        password = groupPass.getText().toString();
+        if ( email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(getApplicationContext(),"Fill this thing up!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Check your creds!",
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/kodbale/dkode/MainActivity.java
@@ -0,0 +1,25 @@
+package com.kodbale.dkode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.kodbale.dkode.Login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        if ( user == null){
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+    }
+}
